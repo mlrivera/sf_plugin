b17d58f723564644dee784b366b6c990a00ae6e3
mlrivera <marialauren.rivera@mail.utoronto.ca>
2016-08-18 10:07:37 -0400

    Setting up files
    Set up plugin file and style.css

---
+++ plugin.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+*Plugin Name: Simfolio Plugin
+*Plugin URI:
+*Description:
+*Author: 
+*Author URI:
+*Version 1.0
+*/
+
+
+//Create Widget
+
+//Create Custom Post Type
+
+//Create Shortcode
