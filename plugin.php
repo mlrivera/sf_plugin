63e993684504e39fe528aa153930402fc1223438
mlrivera <marialauren.rivera@mail.utoronto.ca>
2016-08-18 23:19:50 -0400

    Adding the widget
    Adding the random quote widget. Referenced widget made in-class.

--- plugin.php
+++ plugin.php
@@ -11,7 +11,57 @@
 
 
 //Create Widget
+class random_quote extends WP_Widget{
+		//sets it up
+		public function __construct(){
+			$widget_info = array(
+				'classname' => 'widget_quotes',
+				'description' => 'Shows one random quote.'
+			);
+			parent::__construct('rand_quote','Random Quote', $widget_info);
+		}
+		//html output on webpage
+		public function widget($args, $instance){
+			$title = apply_filters('widget_title', empty($instance['title']) ? 'Random Quote' : $instance['title'], $instance, $this->id_base);
+            
+			echo $args['before_widget'];
+			
+			if($title){
+				echo $args['before_title'] . $title . $args['after_title'];
+			}?>
+				
+				<div id="rquote"><?php do_shortcode('[rquote]')?>
+                </div>
+				<?php  
+					echo $args['after_widget'];
+					
+					}
+		//backend forms		
+        public function form($instance){
+				$instance = wp_parse_args((array) $instance, array('title'=>''));
+				$title = strip_tags($instance['title']);?>
+						
+				<p>
+				    <label for="<?php echo $this->get_field_id('title'); ?>">Title:</label> 
+				    <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr($title); ?>" />
+                </p>
+							
+            <?php }
+	//updates input
+        public function update($new_instance,$old_instance){
+					
+					$instance = $old_instance;
+					$new_instance = wp_parse_args((array) $new_instance, array('title' => ''));
+					$instance['title'] = strip_tags($new_instance['title']);
 
+					return $instance;
+						
+				}
+						
+	}
+	
+	add_action('widgets_init',function(){ register_widget('random_quote'); });
+            
 //Create Custom Post Type (CPT)
 function quote_pt() {
   register_post_type( 'quotes',
@@ -30,29 +80,28 @@ function quote_pt() {
 add_action( 'init', 'quote_pt' );
 //Create Shortcode
 
-add_shortcode( 'quote', 'display_custom_post_type' ); //creates a query to loop the custom post type
+add_shortcode( 'rquote', 'show_quotes' ); //creates a query to loop the custom post type
 
-    function display_custom_post_type(){
+    function show_quotes(){
         $args = array(
             'post_type' => 'quotes',
             'post_status' => 'publish',
-            'showposts' => '4'
+            'showposts' => '1',
+            'orderby' => 'rand',
         );
 
-        //$string = '';
         $query = new WP_Query( $args );
         if( $query->have_posts() ){
-            //$string .= '<ul>';
+
             while( $query->have_posts() ){
                 $query->the_post();
-                the_title();
-                the_excerpt();
-                //$string .= '<li>' . 
-                the_post_thumbnail(); 
-                //. '</li>';
+                the_post_thumbnail('thumbnail');?>
+                <h4><?php the_title();?></h4>
+                <p><?php the_excerpt();?></p><?php
             }
-            //$string .= '</ul>';
+
         }
-        wp_reset_postdata();
-        //return $string;
+        wp_reset_postdata(); 
+
+
     }
